71473327dcb5671236ebe7a36c963f9bb5637b94
Yashna Karkera <88830367+yashnakarkera@users.noreply.github.com>
2023-01-16 12:50:38 +0530

    Update main (10).c

--- main (10).c
+++ main (10).c
@@ -1,4 +1,21 @@
-#include <stdio.h>
+
+START
+Procedure Hanoi(disk, source, dest, aux)
+
+   IF disk == 1, THEN
+      move disk from source to dest             
+   ELSE
+      Hanoi(disk - 1, source, aux, dest)     // Step 1
+      move disk from source to dest          // Step 2
+      Hanoi(disk - 1, aux, dest, source)     // Step 3
+   END IF
+   
+END Procedure
+STOP
+
+
+
+#include<stdio.h>
 #include <time.h>
 
 void towerofhanoi(int n,char a,char b,char c)
